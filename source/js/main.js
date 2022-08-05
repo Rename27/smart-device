3bc9626fb418461dc2441388200e64a781c9d962
Rinat Alimov <hostog@gmail.com>
2022-08-06 01:27:27 +0300

    init

---
+++ source/js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+  });
+});
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
