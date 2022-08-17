cafc9fbf3d57fcc51a2dfdf90337eb45f3a3cfe1
Rinat Alimov <hostog@gmail.com>
2022-08-17 21:50:39 +0300

    fix after code-review

--- source/js/modules/modals/modal.js
+++ source/js/modules/modals/modal.js
@@ -1,39 +1,43 @@
-const pageBody = document.querySelector('body');
-const openBtn = pageBody.querySelector('.back-call__button');
-const popup = pageBody.querySelector('.popup');
-const popupOverlay = popup.querySelector('.popup__overlay');
-const closeBtn = popup.querySelector('.popup__close-btn');
-const focusedInput = popup.querySelector('.popup__form input[name="name"]');
+window.addEventListener('load', () => {
+
+  const pageBody = document.querySelector('body');
+  const openBtn = pageBody.querySelector('.back-call__button');
+  const popup = pageBody.querySelector('.popup');
+  const popupOverlay = popup.querySelector('.popup__overlay');
+  const closeBtn = popup.querySelector('.popup__close-btn');
+  const focusedInput = popup.querySelector('.popup__form input[name="name"]');
 
 
-const closePopup = () => {
-  popup.classList.remove('popup--is-active');
-  pageBody.style.overflow = ('auto');
+  const closePopup = () => {
+    popup.classList.remove('popup--is-active');
+    pageBody.style.overflow = ('auto');
 
-};
-
-
-const openPopup = () => {
-  popup.classList.add('popup--is-active');
-  focusedInput.focus();
-  pageBody.style.overflow = ('hidden');
-};
+  };
 
 
-const onEsc = (evt) => {
-  if (evt.key === 'Escape') {
-    closePopup();
-  }
-};
+  const openPopup = () => {
+    popup.classList.add('popup--is-active');
+    focusedInput.focus();
+    pageBody.style.overflow = ('hidden');
+  };
 
 
-const onOverlayClick = (evt) => {
-  if (evt.target === popupOverlay) {
-    closePopup();
-  }
-};
+  const onEsc = (evt) => {
+    if (evt.key === 'Escape') {
+      closePopup();
+    }
+  };
 
-closeBtn.addEventListener('click', closePopup);
-openBtn.addEventListener('click', openPopup);
-document.addEventListener('keydown', onEsc);
-popupOverlay.addEventListener('click', onOverlayClick);
+
+  const onOverlayClick = (evt) => {
+    if (evt.target === popupOverlay) {
+      closePopup();
+    }
+  };
+
+
+  closeBtn.addEventListener('click', closePopup);
+  openBtn.addEventListener('click', openPopup);
+  popupOverlay.addEventListener('click', onOverlayClick);
+  document.addEventListener('keydown', onEsc);
+});
--- source/js/modules/show-more.js
+++ source/js/modules/show-more.js
@@ -1,82 +1,88 @@
-const navBtnShowMore = document.querySelector('.nav__btn-show-more');
-const navContainer = document.querySelector('.nav__wrapper');
+window.addEventListener('DOMContentLoaded', () => {
 
-const footerContactsBtnShowMore = document.querySelector('.footer-contacts__btn-show-more');
-const footerContactsContainer = document.querySelector('.footer-contacts__list');
-
-const aboutDescriptionsContainer = document.querySelector('.about__descriptions');
-const aboutBtnShowMore = document.querySelector('.about__wrapper button');
-
-
-navBtnShowMore.classList.remove('nav__btn-show-more--js-none');
-navContainer.classList.remove('nav__wrapper--js-none');
-
-footerContactsBtnShowMore.classList.remove('footer-contacts__btn-show-more--js-none');
-footerContactsContainer.classList.remove('footer-contacts__list--js-none');
-
-aboutDescriptionsContainer.classList.remove('about__descriptions--show-more');
-aboutBtnShowMore.style.display = 'block';
-
-const navContainerClose = () => {
-  navBtnShowMore.classList.remove('nav__btn-show-more--is-open');
-  navBtnShowMore.classList.add('nav__btn-show-more--is-close');
-  navContainer.classList.add('nav__wrapper--is-close');
-  navContainer.classList.remove('nav__wrapper--is-open');
-
-};
-const navContainerOpen = () => {
-  navBtnShowMore.classList.remove('nav__btn-show-more--is-close');
-  navBtnShowMore.classList.add('nav__btn-show-more--is-open');
-  navContainer.classList.remove('nav__wrapper--is-close');
-  navContainer.classList.add('nav__wrapper--is-open');
-};
-
-const footerContainerClose = () => {
-  footerContactsBtnShowMore.classList.remove('footer-contacts__btn-show-more--is-open');
-  footerContactsBtnShowMore.classList.add('footer-contacts__btn-show-more--is-close');
-  footerContactsContainer.classList.add('footer-contacts__list--is-close');
-  footerContactsContainer.classList.remove('footer-contacts__list--is-open');
-};
-const footerContainerOpen = () => {
-  footerContactsBtnShowMore.classList.remove('footer-contacts__btn-show-more--is-close');
-  footerContactsBtnShowMore.classList.add('footer-contacts__btn-show-more--is-open');
-  footerContactsContainer.classList.remove('footer-contacts__list--is-close');
-  footerContactsContainer.classList.add('footer-contacts__list--is-open');
-};
-
-const navShowMore = () => {
-  if (navBtnShowMore.classList.contains('nav__btn-show-more--is-close')) {
-    navContainerOpen();
-    footerContainerClose();
-
-  } else {
-    navContainerClose();
+  const navBtnShowMore = document.querySelector('.nav__btn-show-more');
+  const navContainer = document.querySelector('.nav__wrapper');
+  if (navBtnShowMore.classList.contains('nav__btn-show-more--js-none')) {
+    navBtnShowMore.classList.remove('nav__btn-show-more--js-none');
+    navContainer.classList.remove('nav__wrapper--js-none');
   }
-};
 
 
-const footerContactsShowMore = () => {
-  if (footerContactsBtnShowMore.classList.contains('footer-contacts__btn-show-more--is-close')) {
-    footerContainerOpen();
-    navContainerClose();
-
-  } else {
-    footerContainerClose();
+  const footerContactsBtnShowMore = document.querySelector('.footer-contacts__btn-show-more');
+  const footerContactsContainer = document.querySelector('.footer-contacts__list');
+  if (footerContactsBtnShowMore.classList.contains('footer-contacts__btn-show-more--js-none')) {
+    footerContactsBtnShowMore.classList.remove('footer-contacts__btn-show-more--js-none');
+    footerContactsContainer.classList.remove('footer-contacts__list--js-none');
   }
-};
 
 
-const aboutDescriptionShowMore = () => {
-  if (aboutDescriptionsContainer.classList.contains('about__descriptions--show-more')) {
-    aboutDescriptionsContainer.classList.remove('about__descriptions--show-more');
-    aboutBtnShowMore.textContent = 'Подробнее';
-  } else {
-    aboutDescriptionsContainer.classList.add('about__descriptions--show-more');
-    aboutBtnShowMore.textContent = 'Скрыть';
+  const aboutDescriptionsContainer = document.querySelector('.about__descriptions');
+  const aboutBtnShowMore = document.querySelector('.about__wrapper button');
+  if (aboutDescriptionsContainer) {
+    if (aboutDescriptionsContainer.classList.contains('about__descriptions--show-more')) {
+      aboutDescriptionsContainer.classList.remove('about__descriptions--show-more');
+      aboutBtnShowMore.style.display = 'block';
+    }
   }
-};
+
+  const navContainerClose = () => {
+    navBtnShowMore.classList.remove('nav__btn-show-more--is-open');
+    navBtnShowMore.classList.add('nav__btn-show-more--is-close');
+    navContainer.classList.add('nav__wrapper--is-close');
+    navContainer.classList.remove('nav__wrapper--is-open');
+
+  };
+  const navContainerOpen = () => {
+    navBtnShowMore.classList.remove('nav__btn-show-more--is-close');
+    navBtnShowMore.classList.add('nav__btn-show-more--is-open');
+    navContainer.classList.remove('nav__wrapper--is-close');
+    navContainer.classList.add('nav__wrapper--is-open');
+  };
+  const navShowMore = () => {
+    if (navBtnShowMore.classList.contains('nav__btn-show-more--is-close')) {
+      navContainerOpen();
+      footerContainerClose();
+
+    } else {
+      navContainerClose();
+    }
+  };
 
 
-navBtnShowMore.addEventListener('click', navShowMore);
-footerContactsBtnShowMore.addEventListener('click', footerContactsShowMore);
-aboutBtnShowMore.addEventListener('click', aboutDescriptionShowMore);
+  const footerContainerClose = () => {
+    footerContactsBtnShowMore.classList.remove('footer-contacts__btn-show-more--is-open');
+    footerContactsBtnShowMore.classList.add('footer-contacts__btn-show-more--is-close');
+    footerContactsContainer.classList.add('footer-contacts__list--is-close');
+    footerContactsContainer.classList.remove('footer-contacts__list--is-open');
+  };
+  const footerContainerOpen = () => {
+    footerContactsBtnShowMore.classList.remove('footer-contacts__btn-show-more--is-close');
+    footerContactsBtnShowMore.classList.add('footer-contacts__btn-show-more--is-open');
+    footerContactsContainer.classList.remove('footer-contacts__list--is-close');
+    footerContactsContainer.classList.add('footer-contacts__list--is-open');
+  };
+  const footerContactsShowMore = () => {
+    if (footerContactsBtnShowMore.classList.contains('footer-contacts__btn-show-more--is-close')) {
+      footerContainerOpen();
+      navContainerClose();
+
+    } else {
+      footerContainerClose();
+    }
+  };
+
+
+  const aboutDescriptionShowMore = () => {
+    if (aboutDescriptionsContainer.classList.contains('about__descriptions--show-more')) {
+      aboutDescriptionsContainer.classList.remove('about__descriptions--show-more');
+      aboutBtnShowMore.textContent = 'Подробнее';
+    } else {
+      aboutDescriptionsContainer.classList.add('about__descriptions--show-more');
+      aboutBtnShowMore.textContent = 'Скрыть';
+    }
+  };
+
+  navBtnShowMore.addEventListener('click', navShowMore);
+  footerContactsBtnShowMore.addEventListener('click', footerContactsShowMore);
+  aboutBtnShowMore.addEventListener('click', aboutDescriptionShowMore);
+});
